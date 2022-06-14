38a1f33b94f17895ff2a69de9fcbaf065a0a40a9
LuisElPoti <adames1601@gmail.com>
2022-06-14 16:02:02 -0400

    cambios

---
+++ CitasHospital/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CitasHospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ptbFondo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgDatos = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDoctor = new System.Windows.Forms.TextBox();
+            this.txtCentro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbHora = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbFondo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbFondo
+            // 
+            this.ptbFondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ptbFondo.Image = ((System.Drawing.Image)(resources.GetObject("ptbFondo.Image")));
+            this.ptbFondo.Location = new System.Drawing.Point(0, 0);
+            this.ptbFondo.Name = "ptbFondo";
+            this.ptbFondo.Size = new System.Drawing.Size(807, 437);
+            this.ptbFondo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbFondo.TabIndex = 2;
+            this.ptbFondo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(278, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Citas médicas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 9;
+            // 
+            // dgDatos
+            // 
+            this.dgDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgDatos.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDatos.Location = new System.Drawing.Point(38, 305);
+            this.dgDatos.Name = "dgDatos";
+            this.dgDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgDatos.Size = new System.Drawing.Size(728, 132);
+            this.dgDatos.TabIndex = 12;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 124);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(174, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Doctor:";
+            // 
+            // txtDoctor
+            // 
+            this.txtDoctor.Location = new System.Drawing.Point(117, 179);
+            this.txtDoctor.Name = "txtDoctor";
+            this.txtDoctor.Size = new System.Drawing.Size(174, 20);
+            this.txtDoctor.TabIndex = 7;
+            // 
+            // txtCentro
+            // 
+            this.txtCentro.Location = new System.Drawing.Point(592, 125);
+            this.txtCentro.Name = "txtCentro";
+            this.txtCentro.Size = new System.Drawing.Size(174, 20);
+            this.txtCentro.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(439, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Centro Médico:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(491, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Telefono:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(592, 179);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(174, 20);
+            this.txtNumero.TabIndex = 14;
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAñadir.Location = new System.Drawing.Point(550, 276);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(75, 23);
+            this.btnAñadir.TabIndex = 16;
+            this.btnAñadir.Text = "Añadir";
+            this.btnAñadir.UseVisualStyleBackColor = false;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(38, 229);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(528, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 24);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Hora:";
+            // 
+            // cbHora
+            // 
+            this.cbHora.FormattingEnabled = true;
+            this.cbHora.Location = new System.Drawing.Point(592, 225);
+            this.cbHora.Name = "cbHora";
+            this.cbHora.Size = new System.Drawing.Size(121, 21);
+            this.cbHora.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 437);
+            this.Controls.Add(this.cbHora);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgDatos);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCentro);
+            this.Controls.Add(this.txtDoctor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.ptbFondo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbFondo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbFondo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgDatos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDoctor;
+        private System.Windows.Forms.TextBox txtCentro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbHora;
+    }
+}
+
---
+++ CitasHospital/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CitasHospital
+{
+    public partial class Form1 : Form
+    {
+        List<Appoiment> Appoiments = new List<Appoiment>();
+        List<string> Horas = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            fillHours();
+        }
+
+        private void fillHours()
+        {   
+            Horas = Enumerable.Range(08, 15).Select(i => (DateTime.MinValue.AddHours(i)).ToString("hh.mm tt")).ToList();
+            
+            cbHora.DataSource = Horas;
+        }
+
+
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            SaveAppoiment();
+            EmptyControls();
+            DefaultValues();
+
+
+        }
+
+        private void SaveAppoiment()
+        {
+            var rnd = new Random();
+
+            var appoiment = new Appoiment
+            {
+                Id = Guid.NewGuid(),
+                Code = rnd.Next(100000, 999999).ToString(),
+                Nombre = txtNombre.Text,
+                Telefono = txtNumero.Text,
+                Doctor = txtDoctor.Text,
+                Centro = txtCentro.Text,
+                ScheduleDate = dtpDate.Value,
+                ScheduleHour = cbHora.SelectedValue.ToString(),
+                CreateDate = DateTime.Now
+
+
+
+            };
+
+            var existAppoiment = Appoiments.Count(x => x.ScheduleDate.ToString("dd/mm/yyyy") == dtpDate.Value.ToString("dd/mm/yyyy") && x.ScheduleHour == cbHora.SelectedValue.ToString());
+            if (existAppoiment > 0)
+            {
+                MessageBox.Show("Horario no disponible");
+                return;
+            }
+            Appoiments.Add(appoiment);
+
+            var selectedHora = Horas.FirstOrDefault(x => x == cbHora.SelectedValue.ToString());
+            Horas.Remove(selectedHora);
+            cbHora.DataSource = null;
+            cbHora.DataSource = Horas;  
+
+            GetAppoiments();
+            EmptyControls();
+        }
+
+        
+        
+
+            private void GetAppoiments()
+        {
+            dgDatos.DataSource = null;
+            dgDatos.DataSource = Appoiments;
+
+            dgDatos.Columns["Id"].Visible = false;
+        }
+
+        private void DefaultValues()
+        {
+            dtpDate.Value = DateTime.Now;
+           
+            
+        }
+
+        private void EmptyControls()
+        {
+            txtCentro.Text = String.Empty;
+            txtDoctor.Text = String.Empty;  
+            txtNombre.Text = String.Empty;  
+            txtNumero.Text = String.Empty;
+        }
+
+        
+    }
+
+    public class Appoiment
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+        public string Nombre { get; set;}
+        public string Telefono { get; set;}
+        public string Centro { get; set;}
+        public string Doctor { get; set;}
+        public string ScheduleHour { get; set;}
+        
+        public DateTime ScheduleDate { get; set;}
+        public DateTime CreateDate { get; set;}
+
+
+
+
+    }
+
+    
+}
